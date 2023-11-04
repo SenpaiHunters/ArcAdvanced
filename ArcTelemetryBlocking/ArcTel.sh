d74367dc84aaa8e4902981d2f8f2e776834feace
Kami on YT <kamibusinessinquirers@gmail.com>
2023-11-04 13:42:48 +1100

    Update ArcTel.sh

--- ArcTelemetryBlocking/ArcTel.sh
+++ ArcTelemetryBlocking/ArcTel.sh
@@ -1,79 +1,52 @@
 #!/bin/bash
 
 block_domains() {
-    domains=(
-        "launchdarkly.com"
-        "mobile.launchdarkly.com"
-        "clientstream.launchdarkly.com"
-        "segment.io"
-        "api.segment.io"
-        "sentry.io"
-        "0298668.ingest.sentry.io"
-        "segment.com"
-        "cdn-settings.segment.com"
-    )
+	domains=(
+		"launchdarkly.com"
+		"mobile.launchdarkly.com"
+		"clientstream.launchdarkly.com"
+		"segment.io"
+		"api.segment.io"
+		"sentry.io"
+		"0298668.ingest.sentry.io"
+		"segment.com"
+		"cdn-settings.segment.com"
+	)
 
-    success=true
+	if [ -f /etc/hosts.backup ]; then
+		echo -e "\033[31m❌ Domains are already blocked.\033[0m"
+		return
+	fi
 
-    for domain in "${domains[@]}"; do
-        echo "Blocking $domain..."
-        echo "127.0.0.1 $domain" | sudo tee -a /etc/hosts >/dev/null
+	echo -e "\033[32m✅ Blocking domains...\033[0m"
+	sudo cp /etc/hosts /etc/hosts.backup
 
-        if ping -c 1 $domain | grep -q "PING $domain (127.0.0.1)"; then
-            echo -e "\\033[32m✅ $domain – Blocked Successfully\\033[0m"
-        else
-            echo -e "\\033[31m❌ $domain – Not Blocked\\033[0m"
-            success=false
-        fi
-    done
+	for domain in "${domains[@]}"; do
+		echo -e "\033[32m✅ Blocking $domain...\033[0m"
+		echo "127.0.0.1 $domain" | sudo tee -a /etc/hosts >/dev/null
+	done
 
-    if $success; then
-        echo -e "\\033[32mAll domains are now blocked. Your privacy is protected!\\033[0m"
-    else
-        echo -e "\\033[31mSome domains failed to block. Please check your system configuration.\\033[0m"
-    fi
+	echo -e "\033[32mAll domains are now blocked.\033[0m"
+	echo "To unblock these domains, run: $0 unblock"
 }
 
 unblock_domains() {
-    domains=(
-        "launchdarkly.com"
-        "mobile.launchdarkly.com"
-        "clientstream.launchdarkly.com"
-        "segment.io"
-        "api.segment.io"
-        "sentry.io"
-        "0298668.ingest.sentry.io"
-        "segment.com"
-        "cdn-settings.segment.com"
-    )
-
-    success=true
-
-    for domain in "${domains[@]}"; do
-        echo "Unblocking $domain..."
-        sudo sed -i '' "/$domain/d" /etc/hosts
-
-        if ! ping -c 1 $domain | grep -q "PING $domain (127.0.0.1)"; then
-            echo -e "\\033[32m✅ $domain – Unblocked Successfully\\033[0m"
-        else
-            echo -e "\\033[31m❌ $domain – Not Unblocked\\033[0m"
-            success=false
-        fi
-    done
-
-    if $success; then
-        echo -e "\\033[32mAll domains are now unblocked. Your internet access is restored!\\033[0m"
-    else
-        echo -e "\\033[31mSome domains failed to unblock. Please check your system configuration.\\033[0m"
-    fi
+	if [ -f /etc/hosts.backup ]; then
+		echo -e "\033[32m✅ Unblocking domains...\033[0m"
+		sudo mv /etc/hosts.backup /etc/hosts
+		for domain in "${domains[@]}"; do
+			echo -e "\033[32m✅ Unblocking $domain...\033[0m"
+		done
+		echo "To block these domains again, run: $0 block"
+	else
+		echo -e "\033[31m❌ Domains are not currently blocked.\033[0m"
+	fi
 }
 
 if [ "$1" == "block" ]; then
-    echo "Blocking telemetry domains..."
-    block_domains
+	block_domains
 elif [ "$1" == "unblock" ]; then
-    echo "Unblocking telemetry domains..."
-    unblock_domains
+	unblock_domains
 else
-    echo "Usage: $0 [block|unblock]"
+	echo "Usage: $0 [block|unblock]"
 fi
