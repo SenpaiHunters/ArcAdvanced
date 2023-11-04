c688a45c91447704d65abdf92f50da9c7e9c51e4
Kami on YT <kamibusinessinquirers@gmail.com>
2023-11-04 13:50:19 +1100

    Update ArcTel.sh

--- ArcTelemetryBlocking/ArcTel.sh
+++ ArcTelemetryBlocking/ArcTel.sh
@@ -1,45 +1,54 @@
 #!/bin/bash
 
-block_domains() {
-	domains=(
-		"launchdarkly.com"
-		"mobile.launchdarkly.com"
-		"clientstream.launchdarkly.com"
-		"segment.io"
-		"api.segment.io"
-		"sentry.io"
-		"0298668.ingest.sentry.io"
-		"segment.com"
-		"cdn-settings.segment.com"
-	)
+# Define the domains array
+domains=(
+	"launchdarkly.com"
+	"mobile.launchdarkly.com"
+	"clientstream.launchdarkly.com"
+	"segment.io"
+	"api.segment.io"
+	"sentry.io"
+	"0298668.ingest.sentry.io"
+	"segment.com"
+	"cdn-settings.segment.com"
+)
 
+# Define color codes and messages
+red="\033[31m"
+green="\033[32m"
+reset="\033[0m"
+blocked_msg="${green}✅ Blocking domain:${reset}"
+unblocked_msg="${green}✅ Unblocking domain:${reset}"
+not_blocked_msg="${red}❌ Domains are not currently blocked.${reset}"
+already_blocked_msg="${red}❌ Domains are already blocked.${reset}"
+
+block_domains() {
 	if [ -f /etc/hosts.backup ]; then
-		echo -e "\033[31m❌ Domains are already blocked.\033[0m"
+		echo -e "$already_blocked_msg"
 		return
 	fi
 
-	echo -e "\033[32m✅ Blocking domains...\033[0m"
 	sudo cp /etc/hosts /etc/hosts.backup
 
 	for domain in "${domains[@]}"; do
-		echo -e "\033[32m✅ Blocking $domain...\033[0m"
+		echo -e "$blocked_msg $domain"
 		echo "127.0.0.1 $domain" | sudo tee -a /etc/hosts >/dev/null
 	done
 
-	echo -e "\033[32mAll domains are now blocked.\033[0m"
+	echo -e "${green}All domains are now blocked.${reset}"
 	echo "To unblock these domains, run: $0 unblock"
 }
 
 unblock_domains() {
 	if [ -f /etc/hosts.backup ]; then
-		echo -e "\033[32m✅ Unblocking domains...\033[0m"
+		echo -e "$unblocked_msg"
 		sudo mv /etc/hosts.backup /etc/hosts
 		for domain in "${domains[@]}"; do
-			echo -e "\033[32m✅ Unblocking $domain...\033[0m"
+			echo -e "$unblocked_msg $domain"
 		done
 		echo "To block these domains again, run: $0 block"
 	else
-		echo -e "\033[31m❌ Domains are not currently blocked.\033[0m"
+		echo -e "$not_blocked_msg"
 	fi
 }
 
