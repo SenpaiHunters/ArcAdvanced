af40049d34027b635242b6b354f2c045b2ab16da
Kami on YT <kamibusinessinquirers@gmail.com>
2023-11-04 22:55:49 +1100

    Update ArcTel.sh

--- ArcTelemetryBlocking/ArcTel.sh
+++ ArcTelemetryBlocking/ArcTel.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-# Define the URL
-arc_tel_url="https://raw.githubusercontent.com/SenpaiHunters/ArcAdvanced/main/ArcTelemetryBlocking/ArcTel.sh"
+# Constants
+ARC_TEL_URL="https://raw.githubusercontent.com/SenpaiHunters/ArcAdvanced/main/ArcTelemetryBlocking/ArcTel.sh"
+HOSTS_FILE="/etc/hosts"
+BACKUP_FILE="/etc/hosts.backup"
 
-# Define the domains array
+# Define the domains array as a regular indexed array
 domains=(
 	"launchdarkly.com"
 	"mobile.launchdarkly.com"
@@ -12,62 +14,69 @@ domains=(
 	"api.segment.io"
 	"sentry.io"
 	"*.ingest.sentry.io"
-	# "0298668.ingest.sentry.io"
 	"segment.com"
 	"cdn-settings.segment.com"
 )
 
-# Define color codes and messages
-red="\033[31m"
-green="\033[32m"
+# Colors
+purple="\033[94m" # Purple
+red="\033[91m" # Red
+white="\033[97m" # White
+green="\033[92m" # Green
+blue="\033[94m" # Blue
 reset="\033[0m"
-blocked_msg="${green}✅ Blocking domain:${reset}"
-unblocked_msg="${green}✅ Unblocking domain:${reset}"
-not_blocked_msg="${red}❌ Domains are not currently blocked.${reset}"
-already_blocked_msg="${red}❌ Domains are already blocked.${reset}"
+
+# Functions
+error() {
+	echo -e "${red}Error: $1${reset}" 1>&2
+	exit 1
+}
+
+create_backup() {
+	if [ -f "$BACKUP_FILE" ]; then
+		error "Backup file $BACKUP_FILE already exists."
+	fi
+
+	if ! sudo cp "$HOSTS_FILE" "$BACKUP_FILE"; then
+		error "Failed to create a backup of $HOSTS_FILE."
+	fi
+}
 
 block_domains() {
-	if [ -f /etc/hosts.backup ]; then
-		echo -e "$already_blocked_msg"
+	if [ -f "$BACKUP_FILE" ]; then
+		error "Domains are already blocked."
 		return
 	fi
 
-	if ! sudo cp /etc/hosts /etc/hosts.backup; then
-		echo -e "${red}Error: Failed to create a backup of /etc/hosts.${reset}"
-		exit 1
-	fi
+	create_backup
 
 	for domain in "${domains[@]}"; do
-		echo -e "$blocked_msg $domain"
-		if ! echo "127.0.0.1 $domain" | sudo tee -a /etc/hosts >/dev/null; then
-			echo -e "${red}Error: Failed to add $domain to /etc/hosts.${reset}"
-			exit 1
+		echo -e "${green}✅ Blocking domain: ${white}$domain${reset}"
+		if ! echo "127.0.0.1 $domain" | sudo tee -a "$HOSTS_FILE" >/dev/null; then
+			error "Failed to add $domain to $HOSTS_FILE."
 		fi
 	done
 
 	echo -e "${green}All domains are now blocked.${reset}"
-	echo "To unblock these domains, run: curl -s -L $arc_tel_url | bash -s unblock"
+	echo -e "To unblock these domains, run:\n${blue}curl -s -L ${ARC_TEL_URL} | bash -s unblock${reset}"
 }
 
 unblock_domains() {
-	if [ -f /etc/hosts.backup ]; then
-		if ! sudo sed -i -e "/127.0.0.1/ d" /etc/hosts; then
-			echo -e "${red}Error: Failed to unblock domains in /etc/hosts.${reset}"
-			exit 1
-		fi
-
-		if ! sudo mv /etc/hosts.backup /etc/hosts; then
-			echo -e "${red}Error: Failed to restore /etc/hosts from backup.${reset}"
-			exit 1
-		fi
+	if [ -f "$BACKUP_FILE" ]; then
+		for domain in "${domains[@]}"; do
+			sudo sed -i -e "/127.0.0.1 $domain/ d" "$HOSTS_FILE" || error "Failed to unblock domains in $HOSTS_FILE."
+		done
+		sudo mv "$BACKUP_FILE" "$HOSTS_FILE" || error "Failed to restore $HOSTS_FILE from backup"
 
 		for domain in "${domains[@]}"; do
-			echo -e "$unblocked_msg $domain"
+			echo -e "${green}✅ Unblocking domain: ${white}$domain${reset}"
 		done
-		echo "To block these domains, run: curl -s -L $arc_tel_url | bash -s block"
+
+		echo -e "All domains are now unblocked."
+		echo -e "To block these domains, run:\n${blue}curl -s -L ${ARC_TEL_URL} | bash -s block${reset}"
 	else
-		echo -e "$not_blocked_msg"
-		echo "To block these domains, run: curl -s -L $arc_tel_url | bash -s block"
+		echo -e "${red}❌ Domains are not currently blocked.${reset}"
+		echo -e "To block these domains, run:\n${blue}curl -s -L ${ARC_TEL_URL} | bash -s block${reset}"
 	fi
 }
 
@@ -76,4 +85,6 @@ if [ "$1" == "block" ]; then
 	block_domains
 elif [ "$1" == "unblock" ]; then
 	unblock_domains
+else
+	error "Invalid argument. Use 'block' or 'unblock'."
 fi
